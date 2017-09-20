72db5da0cf296657b848e610fea636dd85548af2
lotus <egoo.geovany@gmail.com>
2017-09-20 15:52:27 -0600

    Initial commit

---
+++ app/src/main/java/com/lotus/lateral/ScannerFragment.java
@@ -0,0 +1,56 @@
+package com.lotus.lateral;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ScannerFragment extends Fragment {
+
+
+    public ScannerFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_scanner, container, false);
+
+        Button scan_btn = (Button) view.findViewById(R.id.scan_btn);
+        final Activity activity = getActivity();
+        scan_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("Escaner QR");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+
+            }
+        });
+        return view;
+
+
+    }
+
+
+
+}
